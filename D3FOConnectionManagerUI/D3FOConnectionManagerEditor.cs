dbfcdd410dc53b13e586f32ef59174661bd2838c
sergey104 <sfilperz@gmail.com>
2017-10-05 16:04:13 +0300

    UI class implementation

---
+++ D3FOConnectionManagerUI/D3FOConnectionManagerEditor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D3FOConnectionManager
+{
+    // C# code
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Drawing;
+    using System.Linq;
+    using System.Text;
+    using System.Windows.Forms;
+    using Microsoft.SqlServer.Dts.Design;
+    using Microsoft.SqlServer.Dts.Runtime;
+    using Microsoft.SqlServer.Dts.Runtime.Design;
+
+
+    namespace SSISJoost
+    {
+        public partial class D3FOConnectionManagerEditor : Form
+        {
+            #region General Connection Manager Methods
+            // Setting and getting ConnectionManager
+            private ConnectionManager _connectionManager;
+            public ConnectionManager ConnectionManager
+            {
+                get { return _connectionManager; }
+                set { _connectionManager = value; }
+            }
+
+            // Setting and getting ServiceProvider
+            private IServiceProvider _serviceProvider = null;
+            public IServiceProvider ServiceProvider
+            {
+                get { return _serviceProvider; }
+                set { _serviceProvider = value; }
+            }
+
+            // Default constructor
+            public D3FOConnectionManagerEditor()
+            {
+               // InitializeComponent();
+            }
+
+            public void Initialize(ConnectionManager connectionManager, IServiceProvider serviceProvider)
+            {
+                this._connectionManager = connectionManager;
+                this._serviceProvider = serviceProvider;
+            }
+            #endregion
+
+            #region Page Load
+            // Fill the fields of the form. Get data from connectionManager object
+            private void SMTP2Editor_Load(object sender, EventArgs e)
+            {
+              //  this.txtName.Text = this._connectionManager.Name;
+             //   this.txtDescription.Text = this._connectionManager.Description;
+              //  this.txtURL.Text = this._connectionManager.Properties["URL"].GetValue(_connectionManager).ToString();
+              //  this.txtUserName.Text = this._connectionManager.Properties["UserName"].GetValue(_connectionManager).ToString();
+              //  this.txtPassword.Text = this._connectionManager.Properties["Password"].GetValue(_connectionManager).ToString();
+            }
+            #endregion
+
+            #region Buttons
+            // Save value from fields in connectionManager object
+            private void btnOK_Click(object sender, EventArgs e)
+            {
+              //  this._connectionManager.Name = this.txtName.Text;
+              //  this._connectionManager.Description = this.txtDescription.Text;
+              //  this._connectionManager.Properties["URL"].SetValue(this._connectionManager, this.txtURL.Text);
+              //  this._connectionManager.Properties["UserName"].SetValue(this._connectionManager, this.txtUserName.Text);
+              //  this._connectionManager.Properties["Password"].SetValue(this._connectionManager, this.txtPassword.Text);
+              //  this.DialogResult = DialogResult.OK;
+            }
+
+            // Cancel diolog
+            private void btnCancel_Click(object sender, EventArgs e)
+            {
+                this.DialogResult = DialogResult.Cancel;
+            }
+
+            // Show some helpful information
+            private void btnHelp_Click(object sender, EventArgs e)
+            {
+                MessageBox.Show("Help");
+            }
+            #endregion
+        }
+    }
+}
---
+++ D3FOConnectionManagerUI/D3FOConnectionManagerUI.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SqlServer.Dts.Design;
+using Microsoft.SqlServer.Dts.Runtime;
+using Microsoft.SqlServer.Dts.Runtime.Design;
+
+
+namespace D3FOConnectionManager
+{
+    public class D3FOConnectionManagerUI : IDtsConnectionManagerUI
+    {
+        private ConnectionManager _connectionManager;
+        private IServiceProvider _serviceProvider;
+
+        public D3FOConnectionManagerUI()
+        {
+
+        }
+
+        public void Initialize(ConnectionManager connectionManager, IServiceProvider serviceProvider)
+        {
+            this._connectionManager = connectionManager;
+            this._serviceProvider = serviceProvider;
+        }
+
+
+        public ContainerControl GetView()
+        {
+            D3FOConnectionManagerEditor editor = new D3FOConnectionManagerEditor();
+            editor.ConnectionManager = this._connectionManager;
+            editor.ServiceProvider = this._serviceProvider;
+            return editor;
+        }
+
+        public void Delete(IWin32Window parentWindow)
+        {
+        }
+
+        public void New(IWin32Window parentWindow)
+        {
+        }
+
+        public bool Edit(System.Windows.Forms.IWin32Window parentWindow, Microsoft.SqlServer.Dts.Runtime.Connections connections, Microsoft.SqlServer.Dts.Runtime.Design.ConnectionManagerUIArgs connectionUIArg)
+        {
+            D3FOConnectionManagerEditor editor = new D3FOConnectionManagerEditor();
+
+            editor.Initialize(_connectionManager, this._serviceProvider);
+            if (editor.ShowDialog(parentWindow) == DialogResult.OK)
+            {
+                editor.Dispose();
+                return true;
+            }
+            else
+            {
+                editor.Dispose();
+                return false;
+            }
+        }
+
+        public bool New(System.Windows.Forms.IWin32Window parentWindow, Microsoft.SqlServer.Dts.Runtime.Connections connections, Microsoft.SqlServer.Dts.Runtime.Design.ConnectionManagerUIArgs connectionUIArg)
+        {
+            D3FOConnectionManagerEditor editor = new D3FOConnectionManagerEditor();
+
+            editor.Initialize(_connectionManager, this._serviceProvider);
+            if (editor.ShowDialog(parentWindow) == DialogResult.OK)
+            {
+                editor.Dispose();
+                return true;
+            }
+            else
+            {
+                editor.Dispose();
+                return false;
+            }
+        }
+    }
+}
